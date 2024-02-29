938bedbab64bddcab98e4d502bcedbb5238d3eb2
stam <144084338+c-simon-tam@users.noreply.github.com>
2024-02-29 12:37:02 +0800

    Update ClassSectionTester.java

--- src/ClassSection.java
+++ src/ClassSection.java
@@ -1,5 +1,62 @@
+import java.util.ArrayList;
 
 public class ClassSection {
+    private String subject;
+    private int capacity;
+    int yearLevel;
+    ArrayList<Student> students = new ArrayList<>();
 
+    public ClassSection(String subject,int capacity,int yearLevel) {
+        this.subject = subject;
+        this.capacity = capacity;
+        this.yearLevel = yearLevel;
+        this.students = new ArrayList<Student>();
+    }
+    public String getSubject() {
+        return subject;
+    }
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    public int getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+    public int getYearLevel() {
+        return yearLevel;
+    }
+    public void setYearLevel(int yearLevel) {
+        this.yearLevel = yearLevel;
+    }
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void removeStudent(Student student){
+        this.students.remove(student);
+    }
+    public void addStudent(Student student){
+        if (!isStudentEnrolled(student) && student.getYearLevel() == getYearLevel()) {
+            this.students.add(student);
+        }
+    }
+    public boolean isStudentEnrolled(Student student){
+        for(int i=0;i<students.size();i++){
+            if(student.equals(students.get(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public String toString() {
+        return "ClassSection{subject=" + "'"+subject +"'"+ ", capacity="
+                + capacity + ", yearLevel=" + yearLevel + ", students=" + students+"}";
+        //"Student{id=, firstName='Adrian', lastName='Wong', age=17, yearLevel=12}", idRemoved, "Make sure toString is correct!"
+    }
+    public String toString (ArrayList Students){
+        return(getSubject()+getCapacity()+getYearLevel()+getStudents());
+    }
 
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,54 @@
 public class Student {
+    int id = 0;
+    String firstName;
+    String lastName;
+    int age =0;
+    int yearLevel;
+    static int studentCount;
 
+    public Student(String firstName, String lastName, int age, int yearLevel) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.yearLevel = yearLevel;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public int getAge(){
+        return age;
+    }
+    public int getYearLevel(){
+        return yearLevel;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName= lastName;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setYearLevel(int yearLevel){
+        this.yearLevel = yearLevel;
+    }
+    public boolean equals (Object x){
+        if(super.equals(x)) {
+            return true;
+        }
+        return false;
+    }
+    public String toString(){
+        return "Student{id=" + id + ", firstName=" + "'"+firstName+"'" + ", lastName="
+                + "'"+lastName +"'"+ ", age=" + age + ", yearLevel=" + yearLevel+"}";
+    }
+    //"Student{id=, firstName='Adrian', lastName='Wong', age=17, yearLevel=12}", idRemoved, "Make sure toString is correct!"
 
 }
